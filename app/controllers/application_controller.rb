29691b82e4c2976b9c36b56ab4283621ac68ec1d
Scott Albertson <salbertson@ezpublishing.com>
2010-09-16 12:22:45 -0700

    Added user session navigation. Setup require_user method so we can call before_filter in controllers to require authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,12 @@ class ApplicationController < ActionController::Base
     return @current_user if defined?(@current_user)
     @current_user = current_user_session && current_user_session.user
   end
+
+  def require_user
+    unless current_user
+      flash[:notice] = "You must be logged in."
+      redirect_to login_path
+      return false
+    end
+  end
 end
