8ea7fabf4a5d925a07a8916e54e4164ee5d8b0c1
Scott Albertson <salbertson@ezpublishing.com>
2010-09-18 12:33:50 -0700

    Made posts public.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,4 @@
 class PostsController < ApplicationController
-  before_filter :require_user
-
   def index
     @posts = Post.all
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :require_user, :only => [:edit, :update]
+
   def new
     @user = User.new
   end
