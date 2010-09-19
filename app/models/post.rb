ddb5b2e2bc8007ae2ffffac1801dd165a98d54b6
Scott Albertson <salbertson@ezpublishing.com>
2010-09-18 22:58:30 -0700

    Show empty refactoring message.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :refactorings
   attr_accessible :name, :description, :code
+
+  validates_presence_of :code
 end
--- app/models/refactoring.rb
+++ app/models/refactoring.rb
@@ -2,4 +2,6 @@ class Refactoring < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   attr_accessible :description, :code, :user_id, :post_id
+
+  validates_presence_of :code
 end
