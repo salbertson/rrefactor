66321a61c366d064d93305e55e3fcd48b941cd74
Scott Albertson <salbertson@ezpublishing.com>
2010-09-26 12:59:07 -0700

    DRY'ed up post tests.

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -3,39 +3,38 @@ require 'test_helper'
 class PostTest < ActiveSupport::TestCase
   fixtures :posts
 
+  def setup
+    @post = posts(:valid)
+  end
+
   test "should have a an associated user" do
-    post = posts(:valid)
-    post.user = nil
-    assert !post.valid?
+    @post.user = nil
+    assert !@post.valid?
   end
 
   test "should have a description" do
-    post = posts(:valid)
-    post.description = nil
-    assert !post.valid?
+    @post.description = nil
+    assert !@post.valid?
   end
 
   test "should have code" do
-    post = posts(:valid)
-    post.code = nil
-    assert !post.valid?
+    @post.code = nil
+    assert !@post.valid?
   end
 
   test "should have appropriate length description" do
-    post = posts(:valid)
-    post.description = "a" * 900
-    assert post.valid?
+    @post.description = "a" * 900
+    assert @post.valid?
 
-    post.description = "a" * 2_000
-    assert !post.valid?
+    @post.description = "a" * 2_000
+    assert !@post.valid?
   end
 
   test "should have appropriate length code" do
-    post = posts(:valid)
-    post.code = "a" * 9_000
-    assert post.valid?
+    @post.code = "a" * 9_000
+    assert @post.valid?
 
-    post.description = "a" * 12_000
-    assert !post.valid?
+    @post.description = "a" * 12_000
+    assert !@post.valid?
   end
 end
